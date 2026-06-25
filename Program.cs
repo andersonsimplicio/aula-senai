0ac904300627c433c9b9a514857cdd651070d4ad
GuilhermeGomesResende <0001271678@senaimgaluno.com.br>
2026-06-25 14:57:14 -0300

    Volume cilindro

--- Program.cs
+++ Program.cs
@@ -3,5 +3,13 @@
 class Program{
     static void Main(){
         
+        double raio = 0;
+        double altura = 0;
+        Console.Write("Qual é o seu raio?: ");
+        raio = double.Parse(Console.ReadLine());
+        Console.Write("E qual sua altura?: ");
+        altura = double.Parse(Console.ReadLine());
+        Console.Write($"Seu volume é {(raio*raio)*3.14*altura}");
+
     }
 }
